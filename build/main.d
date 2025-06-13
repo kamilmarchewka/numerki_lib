9f08e74fa56c6989f34ca8c76cfe602817922c4a
Kamil <kamilm2003@gmail.com>
2025-06-13 15:38:43 +0200

    .

--- build/main.d
+++ build/main.d
@@ -1,19 +1,2 @@
-build/main.o: main.cpp include/EulerMethod.hpp include/MetodaBisekcji.hpp \
-  include/MetodaHeuna.hpp include/MetodaNewtona.hpp \
-  include/MetodaPktSr.hpp include/MetodaRK4.hpp \
-  include/MetodaSiecznych.hpp include/PrintMatrix.hpp \
-  include/PrintVector.hpp include/ReadFileToMatrix.hpp \
-  include/RegulaFalsi.hpp include/SolveLU.hpp include/SrBladKwadrat.hpp
-include/EulerMethod.hpp:
-include/MetodaBisekcji.hpp:
-include/MetodaHeuna.hpp:
-include/MetodaNewtona.hpp:
-include/MetodaPktSr.hpp:
-include/MetodaRK4.hpp:
-include/MetodaSiecznych.hpp:
-include/PrintMatrix.hpp:
-include/PrintVector.hpp:
-include/ReadFileToMatrix.hpp:
+build/main.o: main.cpp include/RegulaFalsi.hpp
 include/RegulaFalsi.hpp:
-include/SolveLU.hpp:
-include/SrBladKwadrat.hpp:
--- main.cpp
+++ main.cpp
@@ -1,49 +1,15 @@
 #include <cmath>
 #include <fstream>
-#include <iomanip>
+#include <functional>
 #include <iostream>
-#include <vector>
 
-#include "EulerMethod.hpp"
-#include "MetodaBisekcji.hpp"
-#include "MetodaHeuna.hpp"
-#include "MetodaNewtona.hpp"
-#include "MetodaPktSr.hpp"
-#include "MetodaRK4.hpp"
-#include "MetodaSiecznych.hpp"
-#include "PrintMatrix.hpp"
-#include "PrintVector.hpp"
-#include "ReadFileToMatrix.hpp"
 #include "RegulaFalsi.hpp"
-#include "SolveLU.hpp"
-#include "SrBladKwadrat.hpp"
 
-using namespace std;
+double f1(double x) { return sin(1 / x); }
 
 int main() {
 
-  vector<vector<double>> A;
-  vector<double> b;
-  int N;
-
-  // Wczytanie danych z pliku
-  if (!readFileToMatrix("data.txt", A, b, N)) {
-    return 1;
-  }
-
-  vector<double> x(N, 0);
-  vector<double> z(N, 0);
-
-  cout << "Wczytane dane:" << endl;
-  cout << "N = " << N << endl;
-  cout << "A = " << endl;
-  printMatrix(A, b);
-  printVector("b", b);
-  printVector("x", x);
-  printVector("z", z);
-
-  x = solveLU(A, b, N);
-  printVector("Rozwiazanie", x);
+  find_zeros(f1, 0.0, 0.5, 0.001, "f1.csv");
 
   return 0;
 }
